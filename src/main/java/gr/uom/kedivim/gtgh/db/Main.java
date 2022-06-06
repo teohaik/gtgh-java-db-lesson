a08fbe603509c98312a3b5eae83694b24a7e8f57
theodoros.chaikalis <teohaik@gmail.com>
2022-06-06 14:52:38 +0300

    added examples for birthdate search and insert queries

--- src/main/java/gr/uom/kedivim/gtgh/db/Main.java
+++ src/main/java/gr/uom/kedivim/gtgh/db/Main.java
@@ -2,6 +2,9 @@ package gr.uom.kedivim.gtgh.db;
 
 import gr.uom.kedivim.gtgh.db.model.Person;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,6 +18,46 @@ public class Main {
 
         // searchByAmkaExample(queryEditor);
 
+       // searchByNameExample(queryEditor);
+
+        //searchByBirthdateExample(queryEditor);
+
+        System.out.println("Insert Data");
+        System.out.println("Type user info or 'exit' to exit, ok?");
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        while(!"exit".equalsIgnoreCase(next)){
+            System.out.println("Name: ");
+            String name = scanner.next();
+            System.out.println("Surname");
+            String surname = scanner.next();
+            System.out.println("Amka");
+            String amka = scanner.next();
+            System.out.println("Birthdate in format: DD/MM/YYYY");
+            String birthdateString = scanner.next();
+            LocalDate birthdate = LocalDate.parse(birthdateString, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            Person person = new Person(name, surname, amka, birthdate);
+            queryEditor.insertPerson(person);
+            System.out.println("Continue or exit? ");
+            next = scanner.next();
+        }
+
+    }
+
+    private static void searchByBirthdateExample(QueryEditor queryEditor) {
+        System.out.println("Give minimum age");
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+        int minAge = Integer.parseInt(next);
+
+        List<Person> results = queryEditor.searchPersonGreaterThanAge(minAge);
+
+        System.out.println("Persons greater than "+minAge+" years old: ");
+        for(Person p : results)
+            System.out.println(p);
+    }
+
+    private static void searchByNameExample(QueryEditor queryEditor) {
         System.out.println("Give part of name to search");
 
         Scanner scanner = new Scanner(System.in);
@@ -26,7 +69,6 @@ public class Main {
         for(Person p : personList){
             System.out.println(p);
         }
-
     }
 
     private static void searchByAmkaExample(QueryEditor queryEditor) {
--- src/main/java/gr/uom/kedivim/gtgh/db/QueryEditor.java
+++ src/main/java/gr/uom/kedivim/gtgh/db/QueryEditor.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 public class QueryEditor {
 
-    public static void printPersonsFromDB(){
+    public static void printPersonsFromDB() {
         Connection connection = DBConnector.getConnectionMariaDB();
 
         try {
@@ -21,7 +21,7 @@ public class QueryEditor {
 
             System.out.println("Persons:");
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 System.out.println(name);
             }
@@ -31,18 +31,16 @@ public class QueryEditor {
         }
     }
 
-
-    public List<Person> loadPersonsFromDB(){
+    public List<Person> loadPersonsFromDB() {
 
         List<Person> personList = new ArrayList<>();
 
-        try(Connection connectionMariaDB = DBConnector.getConnectionMariaDB()){
+        try (Connection connectionMariaDB = DBConnector.getConnectionMariaDB()) {
             PreparedStatement preparedStatement =
                     connectionMariaDB.prepareStatement("Select * from person");
 
             personList = extractPersonsFromResults(preparedStatement);
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println("Error while trying to query Persons from DB");
             ex.printStackTrace();
         }
@@ -51,27 +49,26 @@ public class QueryEditor {
 
     public List<Person> searchPersonByAmka(String amkaToSearch) {
 
-        try(Connection connectionMariaDB = DBConnector.getConnectionMariaDB()){
+        try (Connection connectionMariaDB = DBConnector.getConnectionMariaDB()) {
             PreparedStatement ps = connectionMariaDB.prepareStatement("Select * from Person p where p.amka = ?");
             ps.setString(1, amkaToSearch); // is this ok?
 
             List<Person> personList = extractPersonsFromResults(ps);
 
-            if(personList.size() > 1) {
+            if (personList.size() > 1) {
                 throw new RuntimeException("Two persons with same AMKA cannot exist. Error in data!");
             }
             return personList;
-        }
-        catch (Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         throw new RuntimeException("error while trying to search person by amka");
     }
 
-    public List<Person> searchPersonByNameLike(String partOfName){
-        try(Connection conn = DBConnector.getConnectionMariaDB()){
+    public List<Person> searchPersonByNameLike(String partOfName) {
+        try (Connection conn = DBConnector.getConnectionMariaDB()) {
             PreparedStatement ps = conn.prepareStatement("Select * from Person p where p.name like ?");
-            ps.setString(1, "%"+partOfName+"%");
+            ps.setString(1, "%" + partOfName + "%");
 
             List<Person> personList = extractPersonsFromResults(ps);
             return personList;
@@ -82,16 +79,54 @@ public class QueryEditor {
         throw new RuntimeException("error while trying to search person by name");
     }
 
+    public List<Person> searchPersonGreaterThanAge(int minAge) {
+        List<Person> results = new ArrayList<>();
+        try (Connection conn = DBConnector.getConnectionMariaDB()) {
 
+            LocalDate dateToCompare =
+                    LocalDate.of(LocalDate.now().getYear() - minAge,
+                            LocalDate.now().getMonth(),
+                            LocalDate.now().getDayOfMonth());
 
+            PreparedStatement ps =
+                    conn.prepareStatement("Select * from person p where p.birthdate < ?");
 
-    private List<Person> extractPersonsFromResults(PreparedStatement ps){
+            ps.setObject(1, dateToCompare);
+
+            results = extractPersonsFromResults(ps);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return results;
+    }
+
+    public void insertPerson(Person person){
+        try(Connection connection = DBConnector.getConnectionMariaDB()){
+
+            PreparedStatement ps = connection.
+                    prepareStatement("insert into javauser.person(name,surname,amka,birthdate) " +
+                    "values (?,?,?,?)");
+
+            ps.setString(1, person.getName());
+            ps.setString(2, person.getSurname());
+            ps.setString(3, person.getAmka());
+            ps.setObject(4, person.getBirthdate());
+
+            int i = ps.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private List<Person> extractPersonsFromResults(PreparedStatement ps) {
         List<Person> personResults = new ArrayList<>();
 
         try {
             ResultSet resultSet = ps.executeQuery();
 
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 String surname = resultSet.getString("surname");
                 String amka = resultSet.getString("amka");
@@ -107,6 +142,4 @@ public class QueryEditor {
 
         return personResults;
     }
-
-
 }
